809559c8e652415dc10d21799fd6a520e133f5f4
Stroupe79 <cstroupe79@gmail.com>
2019-03-07 16:42:50 -0600

    Getting started

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+ var usrName = prompt("What is your name?");
+ alert("Welcome " + usrName + " to Game of Thrones hangman!");
+
+ var words = [
+    "crown",
+    "hodor",
+    "dragon",
+    "white walker",
+    "stark",
+    "fire"
+ ]
+
+ var answerWord = words[Math.floor(Math.random() * words.length)];
+
+ console.log(answerWord);
