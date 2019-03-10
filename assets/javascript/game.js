f65b0ce568e28c502604c85b556ce71802db58b7
Chris Stroupe <cstroupe79@gmail.com>
2019-03-10 11:02:22 -0500

    scoring working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,8 @@
  ]
  
  var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+ var tries = 5; 
+ var guessSlots;
  //generates random answer word
 
  var word = words[Math.floor(Math.random() * words.length)];
@@ -27,27 +28,36 @@ var blankArray = [];
 for (var i = 0; i < word.length; i++) {
    blankArray.push(" _ ");
 }
+
 document.getElementById("blankSlots").innerHTML = blankArray; 
 
 // User input section and main game loop
 
 
-document.onkeyup = function(event) {
+document.onkeyup = function(loop) {
 
-var userInput = event.key.toLowerCase();
-   alphabet.pull(userInput);
+var userInput = loop.key.toLowerCase();
+   
+
 if (word.indexOf(userInput) === -1 ) {
    alert("Incorrect!");
-}else {
-   alert("Match");
+   tries-- ;
+
+// }else if (word.indexOf(userInput) === ) {
+//    blankArray.push(word.indexOf);
 }
+
 console.log(userInput);
+document.getElementById("tries").innerHTML = tries;
 
 }
+
+
 document.getElementById("alphabet").innerHTML = alphabet;
 
 
 
+// console.log(word);
 console.log(blankArray);
 console.log(alphabet);
 
