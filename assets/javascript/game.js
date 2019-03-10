e6322e7a19a1fdf04f4a9b33d19904f44e7764e4
Chris Stroupe <cstroupe79@gmail.com>
2019-03-09 19:05:37 -0600

    created the answer array to display on the page, currently working on the userinput section

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
- var usrName = prompt("What is your name?");
- alert("Welcome " + usrName + " to Game of Thrones hangman!");
+//  var usrName = prompt("What is your name?");
+//  alert("Welcome " + usrName + " to Game of Thrones hangman!");
+
+
+// variables
 
  var words = [
     "crown",
@@ -9,15 +12,35 @@
     "stark",
     "fire"
  ]
+ 
+ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+ //generates random answer word
 
  var word = words[Math.floor(Math.random() * words.length)];
 
  console.log(word);
-var answer = [];
-for (var i = 0; i < answer.length; i++) {
-   answer[i] = "_";
+
+ //creates the spaces on the html and creates an answer array
+
+var blankArray = [];
+for (var i = 0; i < word.length; i++) {
+   blankArray.push(" _ ");
 }
+document.getElementById("blankSlots").innerHTML = blankArray; 
 
-var letter = prompt("Word chosen. Please choose a letter");
+// User input section
 
+var userInput = prompt("Word chosen. Please choose a letter");
 
+document.onkeyup = function() {
+
+letter = userInput.toLowerCase();
+if (letter === word[1]) {
+   alert("Hello");
+}
+};
+
+console.log(blankArray);
+console.log(alphabet);
+console.log(userInput);
